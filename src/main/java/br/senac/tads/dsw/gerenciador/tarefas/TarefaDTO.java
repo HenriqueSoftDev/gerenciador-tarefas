9f12a72e89a73347bfbddb98d713fe096bfc3d85
Henrique José <henrisilva2003@gmail.com>
2025-09-29 19:56:55 -0300

    Criação da TarefaDTO, com os atributos title, responsavel, dataTermino e details

--- src/main/java/br/senac/tads/dsw/gerenciador/tarefas/TarefaDTO.java
+++ src/main/java/br/senac/tads/dsw/gerenciador/tarefas/TarefaDTO.java
@@ -1,4 +1,67 @@
 package br.senac.tads.dsw.gerenciador.tarefas;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
 public class TarefaDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @NotBlank
+    @Size(min = 1, max = 64)
+    private String responsavel;
+
+    @NotBlank
+    @FutureOrPresent
+    private LocalDate dataTermino;
+
+    @Size(max = 200)
+    private String details;
+
+    public TarefaDTO() {
+    }
+
+    public TarefaDTO(String title, String responsavel, LocalDate dataTermino, String details) {
+        this.title = title;
+        this.responsavel = responsavel;
+        this.dataTermino = dataTermino;
+        this.details = details;
+    }
+
+    public @NotBlank @Size(min = 3, max = 100) String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NotBlank @Size(min = 3, max = 100) String title) {
+        this.title = title;
+    }
+
+    public @NotBlank @Size(min = 1, max = 64) String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(@NotBlank @Size(min = 1, max = 64) String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public @NotBlank @FutureOrPresent LocalDate getDataTermino() {
+        return dataTermino;
+    }
+
+    public void setDataTermino(@NotBlank @FutureOrPresent LocalDate dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+
+    public @Size(max = 200) String getDetails() {
+        return details;
+    }
+
+    public void setDetails(@Size(max = 200) String details) {
+        this.details = details;
+    }
 }

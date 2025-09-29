a1d89259d993fe2340fe8c55eabc43a35187b1a1
Henrique José <henrisilva2003@gmail.com>
2025-09-29 20:32:16 -0300

    Criação do TarefaRestController

--- src/main/java/br/senac/tads/dsw/gerenciador/tarefas/TarefaRestController.java
+++ src/main/java/br/senac/tads/dsw/gerenciador/tarefas/TarefaRestController.java
@@ -1,6 +1,43 @@
 package br.senac.tads.dsw.gerenciador.tarefas;
 
+import java.net.URI;
+import java.util.List;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/tarefas")
 public class TarefaRestController {
 
+    @Autowired
+    private TarefaService service;
 
+    @GetMapping
+    public List<TarefaDTO> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addNew(@RequestBody @Valid TarefaDTO input) {
+        try {
+            TarefaDTO tarefa = service.addNew(input);
+            
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequestUri()
+                    .path("/{title}")
+                    .buildAndExpand(tarefa.getTitle())
+                    .toUri();
+            
+            return ResponseEntity.created(location).body("Tarefa criada com sucesso!");
+
+        } catch (ResponseStatusException ex) {
+            return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
+        }
+    }
 }

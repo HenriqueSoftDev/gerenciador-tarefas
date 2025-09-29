5b77b37ccdea25f8c276d1c967849a8eafad0b30
Henrique José <henrisilva2003@gmail.com>
2025-09-29 20:24:42 -0300

    Criação da TarefaService

--- src/main/java/br/senac/tads/dsw/gerenciador/tarefas/TarefaService.java
+++ src/main/java/br/senac/tads/dsw/gerenciador/tarefas/TarefaService.java
@@ -1,4 +1,43 @@
 package br.senac.tads.dsw.gerenciador.tarefas;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@Service
 public class TarefaService {
+
+    private Map<String, TarefaDTO> mapTarefas;
+
+    public TarefaService(){
+        mapTarefas = new HashMap<>();
+        mapTarefas.put("tarefa", new TarefaDTO("Prova de DSW","Henrique José",LocalDate.parse("2025-09-29"),"Preparar o resumo da prova"));
+    }
+
+    public List<TarefaDTO> findAll() {
+        return new ArrayList<>(mapTarefas.values());
+    }
+
+    public TarefaDTO addNew(TarefaDTO dto) {
+
+        //Validações
+        if (dto.getTitle().isEmpty() || dto.getResponsavel().isEmpty() || dto.getDataTermino() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Os campos de titulo, Responsavel e Data de Termino precisam ser preenchidos!");
+        }
+
+        if (dto.getDataTermino().isBefore(LocalDate.now())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A data de termino não pode ser inferior a data atual!");
+        }
+
+        mapTarefas.put(dto.getTitle(), dto);
+        return dto;
+    }
+
+    public boolean atrasada(TarefaDTO dto){
+        return dto.getDataTermino().isBefore(LocalDate.now());
+    }
+
 }
